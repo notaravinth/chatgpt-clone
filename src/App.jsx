b2baed3b354ef5c9aa8bf780302b213ecb879975
notaravinth <aravinthravichandran2004@gmail.com>
2025-01-29 21:05:11 +0530

    image

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <div className="upperSide">
+          <div className="upperSideTop"><img src="" alt="" className="logo" /><span className="brand"></span></div>
+          <button className="midBtn"><img src="" alt="" className="addBtn" />New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src="" alt="" />What Is Programming?</button>
+            <button className="query"><img src="" alt="" />What Is Programming?</button>
+          </div>
+        </div>
+        <div className="lowerSide">
+
+        </div>
+      </div>
+      <div className="main">
+        
+      </div> 
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
