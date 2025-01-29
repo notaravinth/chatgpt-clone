d1e4e13869754dd571888595d067ef173d830bd0
notaravinth <aravinthravichandran2004@gmail.com>
2025-01-29 17:20:51 +0530

    image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="sidebar">
+        <div className="upperSide">
+          
+        </div>
+        <div className="lowerSide">
+        
+        </div>
+
+      </div>
+      <div className='main'>
+
+      </div>
     </div>
   );
 }
